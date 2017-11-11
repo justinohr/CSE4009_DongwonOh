2c9c70de7562bba7e0036c7f3ab492565da54755
DongwonOh <justinohr@gmail.com>
2017-11-12 00:32:09 +0900

    Signal Assignment

---
+++ signal/stop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+static void sig_fn(int);
+int main(void){
+	struct sigaction act_new;
+	struct sigaction act_old;
+
+	act_new.sa_handler = sig_fn;
+	sigemptyset(&act_new.sa_mask);
+
+	sigaction(SIGINT, &act_new, &act_old); //register handler
+	for(;;) pause();
+}
+
+static void sig_fn(int signo){
+	if(signo == SIGINT)
+		printf("Ctrl-C is pressed. Try Again\n");
+	return;
+}
