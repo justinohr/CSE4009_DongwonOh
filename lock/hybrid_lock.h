6395e6be2fcd6e857ad6ad9b4a82159b1feadce6
DongwonOh <justinohr@gmail.com>
2017-12-13 00:34:29 +0900

    revision

--- lock/hybrid_lock.h
+++ lock/hybrid_lock.h
@@ -3,11 +3,12 @@
 #include <pthread.h>
 #include <sys/time.h>
 
+
 struct hybrid_lock {
 	pthread_mutex_t mutex;
 };
 
-int hybrid_lock_init(hybrid_lock *lock);
-int hybrid_lock_destroy(hybrid_lock *lock);
-int hybrid_lock_lock(hybrid_lock *lock);
-int hybrid_lock_unlock(hybrid_lock *lock);
+int hybrid_lock_init(struct hybrid_lock *lock);
+int hybrid_lock_destroy(struct hybrid_lock *lock);
+int hybrid_lock_lock(struct hybrid_lock *lock);
+int hybrid_lock_unlock(struct hybrid_lock *lock);
