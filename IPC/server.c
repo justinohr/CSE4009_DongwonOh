f87b527f970e44674bddb74f1f0265ecd105c236
DongwonOh <justinohr@gmail.com>
2017-12-25 11:57:55 +0900

    IPC assignment

--- IPC/server.c
+++ IPC/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -24,9 +25,16 @@ int main(void){
 	int que_id[4]; /*using index from 1*/
 	int pid[4]; /*using index from 1*/
 	MsgType msg;
+	void *shm_addr;
+	int shm_id;
 	que_id[1] = msgget(key1, IPC_CREAT|0666);
 	que_id[2] = msgget(key2, IPC_CREAT|0666);
 	que_id[3] = msgget(key3, IPC_CREAT|0666);
+	while((shm_id = shmget(SHMKEY, SHMSIZE, IPC_CREAT|0666)) == -1);
+	if((shm_addr = shmat(shm_id, (void*)0, 0)) == (void*) -1){
+		printf("shmat fail\n");
+		return -1;
+	}
 	printf("Which processes are in this server?\n");
 	for(i = 1; i <= 3; i++){
 		printf("Process %d: ", i);
@@ -34,12 +42,14 @@ int main(void){
 	}
 	int msg_size = sizeof(msg) - sizeof(msg.mtype);
 	int nbytes;
+	int offset = 0;
 	while(1){
 		for(i = 1; i <= 3; i++){
 			for(j = 1; j <= 4; j++){
 				while((nbytes = msgrcv(que_id[i], &msg, msg_size, j <= 3? pid[j] : 1, IPC_NOWAIT)) > 0){
 					if(msg.mtype == 1){
-					/*shared memory*/
+						sprintf((char*)(shm_addr + offset), "%s\n", msg.mtext);
+						offset += strlen(msg.mtext) + 1;
 					}
 					else{
 						for(k = 1; k <= 3; k++){
